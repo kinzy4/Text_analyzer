223c9e737d423cdfdb1208eb4175e6a1f23dc289
Kinzy mohamed <mkinzy71@gmail.com>
2025-12-13 17:28:33 +0200

    Remove old files and move new files into src

--- Types.fs
+++
@@ -1,9 +0,0 @@
-namespace TextAnalyzer
-
-type AnalysisReport = {
-    WordCount: int
-    SentenceCount: int
-    ParagraphCount: int
-    TopWords: (string * int) list
-    AverageSentenceLength: float
-}
